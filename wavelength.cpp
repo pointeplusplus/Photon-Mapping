4eb56de517f67cd24ce2f1a8f7dde3d47cc7a440
Jesse Freitas <freitas.jesse@gmail.com>
2014-10-20 23:54:40 -0700

    wavelength stub and wave vis stub

---
+++ wavelength.cpp
@@ -0,0 +1,30 @@
+#include "vectors.h"
+
+Vec3f wavelengthToRGB(double wavelength){
+	double r = 0, g = 0, b = 0;
+	if(wavelength < 410 && wavelength >= 380){
+
+	}
+	if(wavelength < 440 && wavelength >= 410){
+
+	}
+	if(wavelength < 490 && wavelength >= 440){
+
+	}
+	if(wavelength < 510 && wavelength >= 490){
+
+	}
+	if(wavelength < 580 && wavelength >= 510){
+
+	}
+	if(wavelength < 640 && wavelength >= 580){
+
+	}
+	if(wavelength < 700 && wavelength >= 640){
+
+	}
+	if(wavelength < 780 && wavelength >= 700){
+
+	}
+	return Vec3f(r,g,b);
+}
