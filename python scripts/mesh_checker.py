0e3b6dc837ffea15dd5ad9b362aebe536257d397
Rebecca Nordhauser <rebecca@rpi-wl-269.dynamic.rpi.edu>
2015-05-12 11:35:30 -0400

    removing files from regular directory in favor of src directory

--- python scripts/mesh_checker.py
+++
@@ -1,8 +0,0 @@
-import collections
-for line in open("diamond.obj"):
-	a = line.split(' ')
-	if a[0] == 'f':
-		b= [x for x, y in collections.Counter(a).items() if y > 1]
-		if len(b) > 0:
-			print a,b
-	
